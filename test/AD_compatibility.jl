560cf8fcffe5b59fb6fa53a20d2808cd517a4fbb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-25 16:13:11 +0000

    Snapshot of branch master taken on 2019-09-25T12:13:09.904-04:00 from "https://github.com/UnofficialJuliaMirror/Surrogates.jl-6fc51010-71bc-11e9-0e15-a3fcc6593c49"

--- test/AD_compatibility.jl
+++ test/AD_compatibility.jl
@@ -5,7 +5,9 @@ using Flux
 using Flux: @epochs
 using Flux.Tracker
 using Zygote
+#using Zygote: @nograd
 
+#=
 #FORWARD
 ###### 1D ######
 lb = 0.0
@@ -181,7 +183,7 @@ g([2.0,5.0])
 =#
 
 ### ZYGOTE ###
-
+=#
 ###### 1D ######
 lb = 0.0
 ub = 10.0
@@ -229,9 +231,18 @@ my_loba = LobacheskySurrogate(x,y,α,n,lb,ub)
 g = x -> my_loba'(x)
 g(0.0)
 
+#NN
+Zygote.refresh()
+model = Chain(Dense(1,1))
+loss(x, y) = Flux.mse(model(x), y)
+opt = Descent(0.01)
+n_echos = 1
+my_neural = NeuralSurrogate(x,y,lb,ub,model,loss,opt,n_echos)
+g = x->my_neural'(x)
+g(3.4)
 
 ###### ND ######
-#=
+
 lb = [0.0,0.0]
 ub = [10.0,10.0]
 n = 5
@@ -242,35 +253,51 @@ y = f.(x)
 #Radials
 my_rad = RadialBasis(x,y,[lb,ub],z->norm(z),2)
 g = x -> Zygote.gradient(my_rad,x)
-g([2.0,5.0])
+g((2.0,5.0))
 
 #Kriging
+Zygote.refresh()
 theta = [2.0,2.0]
 p = [1.9,1.9]
 my_krig = Kriging(x,y,p,theta)
 g = x -> Zygote.gradient(my_krig,x)
-g([2.0,5.0])
+g((2.0,5.0))
 
 #Linear Surrogate
+Zygote.refresh()
 my_linear = LinearSurrogate(x,y,lb,ub)
 g = x -> Zygote.gradient(my_linear,x)
-g([2.0,5.0])
+g((2.0,5.0))
 
 #Inverse Distance
+Zygote.refresh()
 p = 1.4
 my_inverse = InverseDistanceSurrogate(x,y,p,lb,ub)
 g = x -> Zygote.gradient(my_inverse,x)
-g([2.0,5.0])
+g((2.0,5.0))
 
-#Lobachesky
+#Lobachesky not working yet weird issue with Zygote @nograd
+#=
+Zygote.refresh()
 alpha = [1.4,1.4]
 n = 4
 my_loba_ND = LobacheskySurrogate(x,y,alpha,n,lb,ub)
 g = x -> Zygote.gradient(my_loba_ND,x)
-g([2.0,5.0])
+g((2.0,5.0))
+=#
 
-#Second order polynomial
+#Second order polynomial mutating arrays
+Zygote.refresh()
 my_second = SecondOrderPolynomialSurrogate(x,y,lb,ub)
 g = x -> Zygote.gradient(my_second,x)
-g([2.0,5.0])
-=#
+g((2.0,5.0))
+
+#NN
+Zygote.refresh()
+model = Chain(Dense(2,1))
+loss(x, y) = Flux.mse(model(x), y)
+opt = Descent(0.01)
+n_echos = 1
+my_neural = NeuralSurrogate(x,y,lb,ub,model,loss,opt,n_echos)
+g = x -> Zygote.gradient(my_neural,x)
+g((2.0,5.0))
